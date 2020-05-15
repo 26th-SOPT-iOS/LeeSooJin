6cba46eb12fe78647c00b9d5aa310b0e4e7fe504
suzieep <wnsuwls0707@gmail.com>
2020-05-16 03:42:38 +0900

    도전과제해따아

--- secondweek/assignment_secondweek_scrollview/assignment_secondweek_scrollview/Scene/View/ViewControllers/KakaoViewController.swift
+++ secondweek/assignment_secondweek_scrollview/assignment_secondweek_scrollview/Scene/View/ViewControllers/KakaoViewController.swift
@@ -15,6 +15,9 @@ class KakaoViewController: UIViewController {
     private var myProfile: [DataInformation] = []
     
     override func viewDidLoad() {
+        tableView.separatorStyle = UITableViewCell.SeparatorStyle.none
+
+
         super.viewDidLoad()
 
         tableView.delegate = self
@@ -24,26 +27,28 @@ class KakaoViewController: UIViewController {
     }
       
     private func setdataInformaitons(){
-        let data1 = DataInformation(name: "이수진", photo: "profile1Img", status: "hi")
-        let data2 = DataInformation(name: "이진수", photo: "profile2Img", status: "hi")
-        let data3 = DataInformation(name: "이수진", photo: "profile3Img", status: "hi")
+        let data1 = DataInformation(name: "황지은", photo: "jieun", status: "과즙미 팡팡~!")
+        let data2 = DataInformation(name: "송지훈", photo: "jihun", status: "아요주정뱅이")
+        let data3 = DataInformation(name: "김민희", photo: "minhee", status: "마라샹궈 먹을사람 구해요")
         let data4 = DataInformation(name: "이진수", photo: "profile4Img", status: "hi")
         let data5 = DataInformation(name: "이수진", photo: "profile5Img", status: "hi")
         let data6 = DataInformation(name: "이진수", photo: "profile6Img", status: "hi")
         let data7 = DataInformation(name: "이수진", photo: "profile7Img", status: "hi")
         let data8 = DataInformation(name: "이진수", photo: "profile8Img", status: "hi")
         let data9 = DataInformation(name: "이진수", photo: "profile9Img", status: "hi")
-        
+        let data10 = DataInformation(name: "이수진", photo: "oursoptBgImg", status: "bye")
         let my = DataInformation(name: "나는이수진", photo: "profile9Img", status: "하힘드러")
 
         
         myProfile = [my]
-        dataInformations = [data1,data2,data3,data4,data5,data6,data7,data8,data9]
+        dataInformations = [data1,data2,data3,data4,data5,data6,data7,data8,data9,data10]
         
     }
 }
 
 extension KakaoViewController: UITableViewDelegate {
+    
+    
     
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
@@ -83,6 +88,17 @@ extension KakaoViewController: UITableViewDelegate {
 
         return view
     }
+    
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+              if editingStyle == .delete {
+                  dataInformations.remove(at: indexPath.row)
+                  tableView.deleteRows(at: [indexPath], with: .fade)
+              } else if editingStyle == .insert {
+                  // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
+              }
+          }
+    
 }
 
 extension KakaoViewController: UITableViewDataSource {
@@ -108,26 +124,22 @@ extension KakaoViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let TableViewCell = tableView.dequeueReusableCell(withIdentifier: TableViewCell.identifier, for:
             indexPath) as? TableViewCell else { return UITableViewCell() }
-//        TableViewCell.profileImage.contentMo
+
         if indexPath.section == 1 {
+            
+//            TableViewCell.underBar.alpha = 0
             TableViewCell.setDataInformation(profileImageName:dataInformations[indexPath.row].photo,
                                          name: dataInformations[indexPath.row].name,
                                          status: dataInformations[indexPath.row].status)
-            
-//            TableViewCell.profileImage.frame = CGRect.init(x: 0, y: 0, width: 30 , height: 30)
-//            let marginguide = contentView.layoutMarginsGuide
+    
 
-                //imageView auto layout constraints
-
-//            TableViewCell.profileImage.translatesAutoresizingMaskIntoConstraints = false
-//
-//            let marginguide = TableViewCell.contentView.layoutMarginsGuide
-//
-             TableViewCell.profileImage.layer.cornerRadius = TableViewCell.profileImage.frame.height/2
+            TableViewCell.profileImage.layer.cornerRadius = 50/2
             TableViewCell.profileImage.topAnchor.constraint(equalTo: TableViewCell.topAnchor, constant: 6).isActive = true
             TableViewCell.profileImage.leadingAnchor.constraint(equalTo: TableViewCell.leadingAnchor, constant: 16).isActive = true
             TableViewCell.profileImage.heightAnchor.constraint(equalToConstant: 50).isActive = true
             TableViewCell.profileImage.widthAnchor.constraint(equalToConstant: 50).isActive = true
+            
+            TableViewCell.profileImage.contentMode = .scaleAspectFill
 
 //            TableViewCell.profileImage.contentMode = .scaleAspectFill
 //            TableViewCell.profileImage.layer.cornerRadius = 20 //half of your width or height
@@ -135,111 +147,24 @@ extension KakaoViewController: UITableViewDataSource {
         
         else {
             TableViewCell.setDataInformation(profileImageName: myProfile[0].photo, name: myProfile[0].name, status: myProfile[0].status)
-//            TableViewCell.profileImage.frame = CGRect.init(x: 0, y: 0, width: 60 , height: 60)
-            
-//            TableViewCell.profileImage.translatesAutoresizingMaskIntoConstraints = false
-//
-//            let marginguide = TableViewCell.contentView.layoutMarginsGuide
-//
-//            TableViewCell.profileImage.topAnchor.constraint(equalTo: marginguide.topAnchor).isActive = true
-//            TableViewCell.profileImage.leadingAnchor.constraint(equalTo: marginguide.leadingAnchor).isActive = true
-            TableViewCell.profileImage.layer.cornerRadius = TableViewCell.profileImage.frame.height/2
+
+//            TableViewCell.profileImage.layer.cornerRadius = TableViewCell.profileImage.frame.height/2
+            TableViewCell.profileImage.layer.cornerRadius = 60/2
             TableViewCell.profileImage.topAnchor.constraint(equalTo: TableViewCell.topAnchor, constant: 13).isActive = true
             TableViewCell.profileImage.leadingAnchor.constraint(equalTo: TableViewCell.leadingAnchor, constant: 16).isActive = true
             TableViewCell.profileImage.heightAnchor.constraint(equalToConstant: 60).isActive = true
             TableViewCell.profileImage.widthAnchor.constraint(equalToConstant: 60).isActive = true
 
-//            TableViewCell.profileImage.contentMode = .scaleAspectFill
+            TableViewCell.profileImage.contentMode = .scaleAspectFill
 //            TableViewCell.profileImage.layer.cornerRadius = 20 //half of your width or height
+
 //
             return TableViewCell
         }
+        
+      
+        
     }
 }
-//func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//
-//    guard let cell = tableView.dequeueReusableCell(withIdentifier: "ProfileCell", for: indexPath) as? ProfileCell else { return UITableViewCell() }
-//
-//    if indexPath.section == 0 {
-//        cell.settings(myProfile[0])
-//        cell.setImageRound(cell.profileImage, section: 0)
-//
-//        cell.profileImage.layer.cornerRadius = cell.profileImage.frame.height/2
-//
-//        return cell
-//    } else {
-//        cell.settings(friends[indexPath.item])
-//        cell.underBar.alpha = 0
-//
-//        return cell
-//    }
-//}
 
 
-
-//extension viewcon_kakao: UITableViewDataSource {
-//
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//
-//        return 2
-//
-//    }
-//
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        switch section
-//        {
-//        case 0 :
-//            return 1
-//        case 1 :
-//            return self.dataInformations.count
-//        default :
-//            return 0
-//        }
-//
-//    }
-//
-//
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//
-//       guard let cell = tableView.dequeueReusableCell(withIdentifier: "ProfileCell", for: indexPath) as? TableViewCell  else { return UITableViewCell() }
-//
-//        if indexPath.section == 0 {
-//            cell.setDataInformation(aaa[0])
-//            cell.profileImage.layer.cornerRadius = cell.profileImage.frame.height/2
-//
-//            return cell
-//        } else {
-//            cell.setDataInformation(aaa[indexPath.item+1])
-//
-//            return cell
-//        }
-//    }
-//
-//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-//        if indexPath.section == 0 {
-//            return 88
-//        } else {
-//            return 66
-//        }
-//    }
-//
-//    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-//
-//        if section == 0 {
-//            return 0
-//        } else {
-//            return 20
-//        }
-//    }
-//
-//
-//}
-//
-//extension viewcon_kakao: UITableViewDelegate {
-//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-//        return 80
-//    }
-//}
-//
-//
-//
