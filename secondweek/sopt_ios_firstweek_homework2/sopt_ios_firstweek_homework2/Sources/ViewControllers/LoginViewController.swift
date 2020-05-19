0a6f3770f0ad77a63768fda914018b02f72a10e5
suzieep <wnsuwls0707@gmail.com>
2020-05-19 19:04:10 +0900

    하드디어회원가입

--- secondweek/sopt_ios_firstweek_homework2/sopt_ios_firstweek_homework2/Sources/ViewControllers/LoginViewController.swift
+++ secondweek/sopt_ios_firstweek_homework2/sopt_ios_firstweek_homework2/Sources/ViewControllers/LoginViewController.swift
@@ -6,33 +6,33 @@
 //  Copyright © 2020 Suzie Lee. All rights reserved.
 //
 
-import UIKit
-
-class LoginViewController: UIViewController {
-    @IBOutlet weak var output_id: UILabel!
-    @IBOutlet weak var output_pw: UILabel!
-    
-    var id: String?
-    var pw: String?
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setLabels()
-        
-        // Do any additional setup after loading the view.
-    }
-    
- 
-    
-    private func setLabels(){
-        guard let id = self.id else{return}
-        guard let pw = self.pw else{return}
-        
-        output_id.text=id
-        output_pw.text=pw
-        
-    }
-    
-    @IBAction func backScreen(_ sender: Any) {self.dismiss(animated: true, completion :nil)
-    }
-}
+//import UIKit
+//
+//class LoginViewController: UIViewController {
+//    @IBOutlet weak var output_id: UILabel!
+//    @IBOutlet weak var output_pw: UILabel!
+//    
+//    var id: String?
+//    var pw: String?
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        setLabels()
+//        
+//        // Do any additional setup after loading the view.
+//    }
+//    
+// 
+//    
+//    private func setLabels(){
+//        guard let id = self.id else{return}
+//        guard let pw = self.pw else{return}
+//        
+//        output_id.text=id
+//        output_pw.text=pw
+//        
+//    }
+//    
+//    @IBAction func backScreen(_ sender: Any) {self.dismiss(animated: true, completion :nil)
+//    }
+//}
--- secondweek/sopt_ios_firstweek_homework2/sopt_ios_firstweek_homework2/Sources/ViewControllers/SignUpViewController.swift
+++ secondweek/sopt_ios_firstweek_homework2/sopt_ios_firstweek_homework2/Sources/ViewControllers/SignUpViewController.swift
@@ -12,10 +12,47 @@ class SignUpViewController: UIViewController {
 
     @IBOutlet weak var signup_id: UITextField!
     @IBOutlet weak var signup_pw: UITextField!
-
+    @IBOutlet weak var signup_name: UITextField!
+    @IBOutlet weak var signup_phone: UITextField!
+    @IBOutlet weak var signup_email: UITextField!
+    
+    
 
     @IBOutlet var buttonCollection: [UIButton]!
     @IBOutlet var textfieldCollection: [UITextField]!
+    
+    @IBAction func Signupbutton(_ sender: Any) {
+        
+        guard let signupid = signup_id.text else{return}
+        guard let signuppw = signup_pw.text else{return}
+        guard let signupname = signup_name.text else{return}
+        guard let signupemail = signup_email.text else{return}
+        guard let signupphone = signup_phone.text else{return}
+        
+        SignupService.shared.signup(id:  signupid, pwd: signuppw, name: signupname , email: signupemail, phone: signupphone ) { networkResult in
+            switch networkResult {
+                
+            case .success(let token):
+                guard let token = token as? String else { return }
+                UserDefaults.standard.set(token, forKey: "token")
+                guard let tabbarController = self.storyboard?.instantiateViewController(identifier:
+                    "customTabbarController") as? UITabBarController else { return }
+                tabbarController.modalPresentationStyle = .fullScreen
+                self.present(tabbarController, animated: true, completion: nil)
+            case .requestErr(let message):
+                guard let message = message as? String else { return }
+                let alertViewController = UIAlertController(title: "로그인 실패", message: message,
+                                                            preferredStyle: .alert)
+                let action = UIAlertAction(title: "확인", style: .cancel, handler: nil)
+                alertViewController.addAction(action)
+                self.present(alertViewController, animated: true, completion: nil)
+                
+            case .pathErr: print("path")
+            case .serverErr: print("serverErr")
+            case .networkFail: print("networkFail")
+            }
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
